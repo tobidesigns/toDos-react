23f145c5cfc6895ccab8127100f5c37984930bff
Tobi <tobiraheem@gmail.com>
2023-03-14 16:58:01 +0000

    Fork todo's repo to add fetch without breaking

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -2,6 +2,21 @@ import React, {useState} from "react";
 import ToDosFunction from "./ToDosFunction.jsx";
 
 
+fetch('https://assets.breatheco.de/apis/fake/todos/user/tobiraheem')
+  .then(response => response.json())
+  .then(data => {data = [...data, 'testing'];
+console.log(data); return data}
+  ).then(moreData => {moreData = [...moreData, 'i think this works but why?']; console.log(moreData)})
+  .catch(error => console.log('error'));
+
+  console.log("================")
+  console.log("================")
+  console.log("================")
+
+  fetch('https://assets.breatheco.de/apis/fake/todos/user/tobiraheem')
+  .then(response => (response.json()).then(data => console.log(data))).catch(console.log("error"))  
+
+
 const Home = () => {
   return (
 		<ToDosFunction/>
